cb51143717bd43e44af7f6c9578f824de97317f3
Tammy Higgins <tmhiggins1986@gmail.com>
2025-07-16 20:23:58 -0500

    Current changes

---
+++ src/component/TableEmployee.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const baseUrl = "http://54.164.109.216:4000/api/";
+const endPoint = "employees";
+
+export const TableEmployee = () => {
+ 
+const [employees, setEmployees] = useState([]);
+
+const getEmployees = async () => {
+  const url = baseUrl + endPoint;
+  const result = await fetch(url);
+  const data = await result.json();
+  setEmployees(data);
+}
+
+useEffect(() => {
+  getEmployees();
+}, []  )
+
+  return (
+  <>
+  <h1>Employee Data Base </h1>
+
+  <table>
+    <thead>
+      <tr>
+  
+       <th>Name</th>
+       <th>department</th> 
+       <th>role</th>
+      </tr>
+    </thead>
+    <tbody>
+      {employees.map((employee) => (
+        <tr key={employee.id}>
+          <td>{employee.name}</td>
+          <td>{employee.department}</td>
+          <td>{employee.role}</td>
+        </tr>
+      ))} 
+     
+    </tbody>
+    </table>
+  </>
+  )
+};
---
+++ src/page/Employee.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { TableEmployee } from '../component/TableEmployee'
+
+export const Employee = () => {
+  return (
+   <TableEmployee></TableEmployee>
+  )
+}
