bd578c0c5530abcf0404c20663fb147ae5f3fe8a
Tammy Higgins <tmhiggins1986@gmail.com>
2025-07-30 20:58:54 -0500

    addind task forms

--- src/component/TaskForm.jsx
+++ src/component/TaskForm.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Employee } from "../page/Employee";
 
-export const TaskForm = () => {
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const endPoint = "phone";
+
+export const PhoneForm = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const { employee_id } = params;
+  const [task, setTask] = useState({
+    employee_id: employee_id,
+    task: "",
+  });
+
+  const formHandler = (event) => {
+    const inputTask = event.target.task;
+    const inputValue = event.target.value;
+    task[inputTask] = inputValue;
+  };
+
+  const submintHandler = async () => {
+    event.preventDefault();
+    const url = `${baseUrl}${endPoint}`;
+    const result = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify([task, employee_id]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await result.json();
+
+    window.location.reload();
+  };
+
+  const returnHandler = () => {
+    navigate("/task/employee_id");
+  };
+
   return (
-    <div>TaskForm</div>
-  )
-}
+    <>
+      <main className="container ml-2 mr-2 mb-5 mt-5">
+        <form onSubmit={submintHandler}>
+          <div className="mb-3">
+            <label className="form-label">Task</label>
+            <input
+              name="task"
+              onChange={formHandler}
+              type="text"
+              className="form-control"
+            />
+          </div>
+
+          <button className="btn btn-primary w-100">Save</button>
+        </form>
+        <button className="btn btn-warning mt-3 w-100" onClick={returnHandler}>
+          Return
+        </button>
+      </main>
+    </>
+  );
+};
--- src/component/TaskTable.jsx
+++ src/component/TaskTable.jsx
@@ -2,25 +2,56 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
-const endPoint = "tasks";
+const endPoint = "task";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-export const TaskTable = async (req, res) => {
-  const [tasks, setTasks] = useState([]);
-
-  const getTasks = async () => {
-    const url = `${baseUrl}${endPoint}/tasks`;
-    const result = await fetch(url);
-    const data = await result.json();
-    setTasks(data);
-  };
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
-  return (
-    <>
-      <h1>Task Table</h1>
-    </>
-  );
+export const TaskForm = () => {
+  const [task, setTasks] = useState([]);
+  const [employee, setEmployees] = useParams().employee_id;
+  const params = useParams;
 };
+const getTasks = async () => {
+  const { employee_id } = params;
+  const url = `${baseUrl}${endPoint}/${id}`;
+  const result = await fetch(url);
+  const data = await result.json();
+  setTasks(data);
+};
+const deleteTask = async (id) => {
+  const url = `${baseUrl}${endPoint}/${id}`;
+  const result = await fetch(url, {
+    method: "DELETE",
+  });
+
+  window.location.reload();
+};  
+useEffect(() => {
+  getTasks();
+}, []);
+
+return (
+  <>
+    <table >
+      <thead>
+        <tr>
+          <th>{employee}</th>
+          <th>Tasks</th>
+        </tr>
+      </thead>
+      <tbody>
+        {task.map((task) => (
+          <tr key={employee.employee_id}>
+            <td>{task}</td>
+            <td>
+              <button
+                className="btn btn-caution"
+                onClick={() => deleteTask(item.task)}
+              ></button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
+);
